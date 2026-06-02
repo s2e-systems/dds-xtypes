42e5758c0f5047e9b52c28c96964a1d4aaef54f2
Angel Martinez <angel@rti.com>
2026-06-02 22:30:10 +0200

    Changing the color to be homogenized

--- generate_xlsx_report.py
+++ generate_xlsx_report.py
@@ -840,7 +840,7 @@ class XlsxReport:
                     str(value.get_supported_tests()) + ' / ' +
                     str(value.get_total_tests()),
                 self.get_format_color(
-                        value.get_passed_tests(), value.get_supported_tests()))
+                        value.get_passed_tests(), value.get_total_tests()))
 
             current_row += 1
 
@@ -896,7 +896,7 @@ class XlsxReport:
                     str(product_value.get_passed_tests()) + ' / ' +
                         str(product_value.get_supported_tests()) + ' / ' +
                         str(product_value.get_total_tests()),
-                    self.get_format_color(product_value.get_passed_tests(), product_value.get_supported_tests()))
+                    self.get_format_color(product_value.get_passed_tests(), product_value.get_total_tests()))
 
     def add_static_data_summary_worksheet(self,
             worksheet: xlsxwriter.Workbook.worksheet_class,
